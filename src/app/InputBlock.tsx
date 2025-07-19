22b1ba4a5abb6628ba9861171f67cdc0599f8c2f
Matthijs Groen <matthijs.groen@gmail.com>
2025-07-19 08:53:48 +0200

    Style input

--- src/app/InputBlock.tsx
+++ src/app/InputBlock.tsx
@@ -1,11 +1,20 @@
 import type { FC } from "react";
+import { useState } from "react";
 import { Block } from "./Block";
 
 export const InputBlock: FC = () => {
+  const [value, setValue] = useState("");
+  const hasValue = value.trim() !== "";
   return (
-    <Block className="bg-blue-100 text-blue-800">
+    <Block
+      className={
+        hasValue ? "bg-green-100 text-green-800" : "bg-blue-100 text-blue-800"
+      }
+    >
       <input
-        type="text"
+        type="number"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
         className="w-full h-full text-center bg-transparent outline-none"
         placeholder="..."
       />
