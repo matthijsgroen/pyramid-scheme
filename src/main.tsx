e0123ea75e6256a86493f9d4be811b4b81f7d802
Matthijs Groen <matthijs.groen@gmail.com>
2025-09-05 16:11:21 +0200

    Using js to fix

--- src/main.tsx
+++ src/main.tsx
@@ -9,3 +9,13 @@ createRoot(document.getElementById("root")!).render(
     <App />
   </StrictMode>
 )
+
+const vh = window.innerHeight * 0.01
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty("--vh", `${vh}px`)
+
+window.addEventListener("resize", () => {
+  // We execute the same script as before
+  const vh = window.innerHeight * 0.01
+  document.documentElement.style.setProperty("--vh", `${vh}px`)
+})
